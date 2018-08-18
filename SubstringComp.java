712201c73c18f0b00453581dd8ef43965ce60e4e
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-18 12:55:51 +0530

    Update SubstringComp.java

--- SubstringComp.java
+++ SubstringComp.java
@@ -7,3 +7,34 @@ Sample Output 0
 ava
 wel
 Solution-
+  import java.util.Scanner;
+
+public class Solution {
+      public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
+        for(int i=0;i<=s.length()-k;i++)
+        {
+            String sub=s.substring(i,i+k);
+            if(smallest.compareTo(sub) > 0 /*|| smallest.isEmpty()*/)
+            {
+                smallest=sub;
+            }
+            if(largest.compareTo(sub) < 0 /*|| largest.isEmpty()*/)
+            {
+                largest=sub;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+  
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
